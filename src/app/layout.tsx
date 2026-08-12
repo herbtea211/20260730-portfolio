8d48452ebab0f3bbb8da011381e46668d543764e
herbtea211 <33927101+herbtea211@users.noreply.github.com>
2026-08-12 09:27:36 +0000

    feat: add hero typography tokens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navigation from "@/components/shell/Navigation";
+import { Roboto } from "next/font/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -10,6 +11,11 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+  const roboto = Roboto({
+  variable: "--font-roboto",
   subsets: ["latin"],
 });
 
@@ -23,10 +29,11 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${roboto.variable} ${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <Navigation></Navigation>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
-// import Image from "next/image";
+import Image from "next/image";
+
 export default function Home() {
   return (
-      <h1>home</h1>
+    <>
+      <section className="flex items-start gap-8">
+        <div>
+          <Image
+            src="/Avatar.png"
+            alt="Sam"
+            width={122}
+            height={122}
+          />
+        </div>
+
+        <div>
+          <h1 className="font-hero font-extraBold text-hero leading-hero tracking-hero">理解問題，設計答案</h1>
+          <p>
+            探索問題本質，以使用者研究、產品策略與系統思維，驅動產品決策。
+          </p>
+        </div>
+      </section>
+    </>
   );
 }
