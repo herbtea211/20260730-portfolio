85e064683c11ec2e6f652cebf1fdb12f13507c26
herbtea211 <33927101+herbtea211@users.noreply.github.com>
2026-08-12 14:04:22 +0000

    add font subContent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 
         <div>
           <h1 className="font-hero font-extraBold text-hero leading-hero tracking-hero">理解問題，設計答案</h1>
-          <p>
+          <p className="font-weight-Medium text-subContent leading-subContent tracking-subContent">
             探索問題本質，以使用者研究、產品策略與系統思維，驅動產品決策。
           </p>
         </div>
