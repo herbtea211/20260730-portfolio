81689279675a8f958046bd3ec083bdf8c82e6e1f
herbtea211 <33927101+herbtea211@users.noreply.github.com>
2026-08-15 10:00:13 +0000

    feat: complete homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,29 +5,28 @@ import IndexCard from "@/components/sections/IndexCard"
 
 export default function Home() {
   return (
-    <>
-      <section className="flex items-start gap-8">
-        <div>
-          <Image
-            src={avatar}
-            alt="Sam"
-            width={122}
-            height={122}
-          />
-        </div>
-
-        <div>
-          <h1 className="font-hero font-extraBold text-hero leading-hero tracking-hero text-textPrimaryColor">理解問題，設計答案</h1>
-          <p className="font-weight-Medium text-subContent leading-subContent tracking-subContent">
-            探索問題本質，以使用者研究、產品策略與系統思維，驅動產品決策。
-          </p>
-        </div>
-      </section>
-      <div>
-        <h2>Seleted Work</h2>
-        <p>每一個設計，都有存在的理由</p>
-      </div>
-      <IndexCard></IndexCard>
-    </>
+          <main className="max-w-[1440px] mx-auto px-[120px] grid grid-cols-8 gap-8 mb-50">
+              <section className="col-start-2 col-span-6 flex items-start gap-8 py-100">
+                <div>
+                  <Image
+                    src={avatar}
+                    alt="Sam"
+                    width={122}
+                    height={122}
+                  />
+                </div>
+                <div>
+                  <h1 className="font-hero font-extraBold text-hero leading-hero tracking-hero text-textPrimaryColor">理解問題，設計答案</h1>
+                  <p className="font-Medium text-subContent leading-subContent tracking-subContent">
+                    探索問題本質，以使用者研究、產品策略與系統思維，驅動產品決策。
+                  </p>
+                </div>
+              </section>
+                <div className="col-span-8 mb-4">
+                  <h2 className=" font-bold text-sectionTitle leading-sectionTitle tracking-sectionTitle">Seleted Work</h2>
+                  <p className="font-Medium text-subContent leading-subContent tracking-subContent text-textTitle">每一個設計，都有存在的理由</p>
+                </div>
+                <IndexCard></IndexCard>
+            </main>
   );
 }
--- src/components/sections/IndexCard.tsx
+++ src/components/sections/IndexCard.tsx
@@ -1,40 +1,36 @@
 import Image from "next/image";
-import avatar from "@/assets/image/index/avatar.png"
+import React from "react";
 
 import { indexCardData } from "@/data/indexCardData"
 import Button from "@/components/ui/Button"
 
 export default function IndexCard() {
-    return (
-        <>
-            {indexCardData.map((item) => {
-                return (
-                        <div
-                        key={item.title}
-                        className="flex items-start gap-8"
-                        >
-                            <Image
-                                src={item.image}
-                                alt={item.title}
-                            />
-                            <div>
-                                <div>
-                                    <h2>{item.title}</h2>
-                                    <p>{item.subtitle}</p>
-                                </div>
-                                <div>
-                                    <p>{item.role}</p>
-                                    <p>{item.description}</p>
-                                    <p>{item.platform}</p>
-                                </div>
-                                <Button showLabel={false} label=""/>
-                            </div>
-                        </div>
-                )
-            })}
-        </>
+  return (
+    <>
+      {indexCardData.map((item) => (
+        <React.Fragment key={item.title}>
+          <Image
+            className="col-start-1 col-span-3"
+            src={item.image}
+            alt={item.title}
+          />
 
-    )
+          <div className="col-start-4 col-span-5">
+            <div className=" mb-12">
+              <h2 className="font-extraBold text-subSectionTitle leading-subSectionTitle tracking-subSectionTitle">{item.title}</h2>
+              <p className="font-Medium text-subContent leading-subContent tracking-subContent text-textTitle">{item.subtitle}</p>
+            </div>
 
+            <div className="mb-9">
+              <p className="font-Medium text-subContent leading-subContent tracking-subContent">{item.role}</p>
+              <p className="font-Medium text-content leading-content tracking-content">{item.description}</p>
+              <p>{item.platform}</p>
+            </div>
 
+            <Button showLabel={false} label="" />
+          </div>
+        </React.Fragment>
+      ))}
+    </>
+  );
 }
