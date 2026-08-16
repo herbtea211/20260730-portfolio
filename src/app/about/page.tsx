938c371de77010bb1d3b239afd124b93b6157ee5
herbtea211 <33927101+herbtea211@users.noreply.github.com>
2026-08-16 00:59:35 +0000

    feat: start about hero

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
+
 export default function AboutPage() {
-    return (
-        <>
-            <h1>about</h1>
-        </>
-    )
+  return (
+    <main>
+      <section className="relative min-h-screen overflow-hidden">
+        <Image
+          src=""
+          alt=""
+          fill
+          priority
+          className="object-cover"
+        />
+
+        <div className="relative mx-auto grid min-h-screen max-w-[1440px] grid-cols-8 gap-x-8 px-[120px]">
+          <h1 className="col-span-6 col-start-2 self-start pt-24 text-white">
+            時間是人生最寶貴的資源。
+            <br />
+            好設計，就是把時間留給最重要的人、事、物。
+          </h1>
+        </div>
+      </section>
+    </main>
+  );
 }
