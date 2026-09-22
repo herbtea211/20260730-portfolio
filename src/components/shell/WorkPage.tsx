4c9479d6f1daffad3b5b4ab3ad5f364e97054f34
herbtea211 <herbtea211@gmail.com>
2026-09-22 13:59:41 +0800

    feat: add work page responsive layout

--- src/components/shell/WorkPage.tsx
+++ src/components/shell/WorkPage.tsx
@@ -10,7 +10,8 @@ export default function WorkPage({ children }: WorkPageProps) {
     <div className="pt-[120px]">
       <WorkSubNavigation />
 
-      <main className="max-w-[1440px] mx-auto px-[120px] grid grid-cols-8 gap-8">
+      {/* <main className="max-w-[1440px] mx-auto px-[120px] grid grid-cols-8 gap-8"> */}
+        <main className="max-w-[1440px] mx-auto px-6 md:px-[120px] grid grid-cols-1 md:grid-cols-8 gap-8">
         {children}
       </main>
     </div>
